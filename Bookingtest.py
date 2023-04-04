c09725de0a65fa0a1a3bad243b118b248f5f5224
Borkan2 <annabork068@gmail.com>
2023-04-04 18:25:56 +0200

    test_Booking.py содержит класс TestMainPage С помощью Selenium и Unittest сделаны тесты: 1.	Открываем страницу booking.com 2.	Находим элемент „поиск” с именем „ss" 3.	Записываем новое значение „    “ 4.	Проверяем кликабельность элемента 5.	Проверяем, что страница перезагрузилась Тесты успешные

---
+++ Bookingtest.py
@@ -0,0 +1,22 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+class TestMainPage(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get("https://www.booking.com/")
+
+    def test_booking(self):
+        driver = self.driver
+        driver.find_element(By.NAME, "ss").click()
+        driver.find_element(By.NAME, "ss").send_keys("Berlin")
+        driver.find_element(By.NAME, "ss").send_keys(Keys.RETURN)
+        self.assertNotEqual(self.driver.current_url, "https://www.booking.com/")
+        name = 'images/' + 'book' + '.png'
+        print(name)
+        driver.save_screenshot(name)
+
+    def tearDown(self):
+        self.driver.quit()
